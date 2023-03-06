ee284c5983ffbdbadcfc785fef7ef5b92e9eef11
Tarun Gupta <tarunthegreat2003@gmail.com>
2023-03-06 22:38:37 +0530

    just some changes

--- app/src/main/java/com/example/cognify/MainActivity2.kt
+++ app/src/main/java/com/example/cognify/MainActivity2.kt
@@ -1,11 +1,33 @@
 package com.example.cognify
 
+import android.animation.Animator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val myAnimationView = findViewById<LottieAnimationView>(R.id.loadingg)
+        myAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {
+                // This method is called when the animation starts
+            }
+            override fun onAnimationEnd(p0: Animator) {
+                // This method is called when the animation ends
+                val intent = Intent(this@MainActivity2, MainActivity3::class.java)
+                startActivity(intent)
+                finish()
+            }
+            override fun onAnimationCancel(p0: Animator) {
+                // This method is called when the animation is canceled
+            }
+            override fun onAnimationRepeat(p0: Animator) {
+                // This method is called when the animation repeats
+            }
+        })
+
     }
 }
