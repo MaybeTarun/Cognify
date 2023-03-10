af817dee6997aa070fb3cb6780d3c51ae7cfe12a
Tarun Gupta <tarunthegreat2003@gmail.com>
2023-03-10 09:36:50 +0530

    just some changes

--- app/src/main/java/com/example/cognify/MainActivity3.kt
+++ app/src/main/java/com/example/cognify/MainActivity3.kt
@@ -2,10 +2,19 @@ package com.example.cognify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+//        val learning_techs = getResources().getStringArray(R.array.learn_tech)
+//        val name = findViewById<TextView>(R.id.tech_name)
+//        val detail = findViewById<TextView>(R.id.tech_detail)
+//
+//        name.setOnClickListener {
+//            detail.visibility = View.VISIBLE
+//        }
     }
 }
---
+++ app/src/main/java/com/example/cognify/learning_tech.java
@@ -0,0 +1,19 @@
+package com.example.cognify;
+
+public class learning_tech {
+    String tech_name;
+    String tech_detail;
+
+    public learning_tech(String tech_name, String tech_detail) {
+        this.tech_name = tech_name;
+        this.tech_detail = tech_detail;
+    }
+
+    public String getTech_name() {
+        return tech_name;
+    }
+
+    public String getTech_detail() {
+        return tech_detail;
+    }
+}
